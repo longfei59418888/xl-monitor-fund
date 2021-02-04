cb6c1269a480202d1505ca0b0913e916e99f9d4c
wangxiaolong-jk <594!longfei>
2021-02-04 17:20:26 +0800

    first commit

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,20 @@
 module.exports = {
     apps: [{
-        name: 'xl-monitor',
-        script: 'index.js',
-        watch: '.'
-    }]
-};
+        name: 'monitor-fund',
+        script: './www/index.js',
+        watch: '.',
+        env_production: {
+            NODE_ENV: 'production'
+        },
+    }],
+    deploy: {
+        production: {
+            user: 'root',
+            host: '42.192.156.205',
+            ref: 'origin/master',       //Git远程/分支
+            repo: 'git@github.com:longfei59418888/xl-monitor-fund.git',
+            path: '/home/items/monitor-fund',
+            "post-deploy": 'npm install && pm2 startOrRestart ecosystem.config.js'
+        }
+    },
+}
