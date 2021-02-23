1b4bd51ee9482af5da7aaebe84b9947e1863679b
wangxiaolong-jk <594!longfei>
2021-02-23 16:56:00 +0800

    first commit

---
+++ configs/metals.js
@@ -0,0 +1,16 @@
+model.exports = [
+    {name: '铝', code: '822'},
+    {name: '铜', code: '820'},
+    {name: '铅', code: '824'},
+    {name: '锌', code: '826'},
+    {name: '镍', code: '830'},
+    {name: '锡', code: '832'},
+    {name: '钴', code: '838'},
+    {name: '硅', code: '844'},
+    {name: '铬', code: '854'},
+    {name: '钢', code: '1360'},
+    {name: '铁', code: '2863'},
+    {name: '金', code: '13'},
+]
+
+// http://app.ometal.cn/data/month_data.asp?pid=1360
